437d1d439d6fc0a2ac36cc7cd0b460554fcad3f4
thegreatkashif <kashif00003088375@gmail.com>
2026-07-21 13:14:49 +0530

    feat(parser): add reverse DNS hostname resolution

--- scanner/parser.py
+++ scanner/parser.py
@@ -1,87 +1,105 @@
+import socket
 from datetime import datetime
 
-from scapy.layers.inet import IP, TCP, UDP, ICMP
-from scapy.layers.inet6 import IPv6, ICMPv6ND_RA
-from scapy.layers.l2 import ARP
+from scapy.all import IP, IPv6, TCP, UDP
 
 
-def get_protocol(packet):
-    if packet.haslayer(ARP):
-        return "ARP"
+HOST_CACHE = {}
 
-    if packet.haslayer(ICMPv6ND_RA):
-        return "ICMPv6"
 
-    if packet.haslayer(ICMP):
-        return "ICMP"
+def resolve_host(ip):
+
+    if ip in HOST_CACHE:
+        return HOST_CACHE[ip]
+
+    try:
+
+        hostname = socket.gethostbyaddr(ip)[0]
+
+    except Exception:
+
+        hostname = ip
+
+    HOST_CACHE[ip] = hostname
+
+    return hostname
+
+
+def protocol_name(packet):
 
     if packet.haslayer(TCP):
-        tcp = packet[TCP]
 
-        if tcp.sport == 80 or tcp.dport == 80:
-            return "HTTP"
+        sport = packet[TCP].sport
+        dport = packet[TCP].dport
 
-        if tcp.sport == 443 or tcp.dport == 443:
-            return "HTTPS"
+        ports = {80: "HTTP", 443: "HTTPS", 22: "SSH", 21: "FTP", 25: "SMTP", 53: "DNS"}
+
+        if sport in ports:
+            return ports[sport]
+
+        if dport in ports:
+            return ports[dport]
 
         return "TCP"
 
     if packet.haslayer(UDP):
-        udp = packet[UDP]
 
-        if udp.sport == 53 or udp.dport == 53:
+        sport = packet[UDP].sport
+        dport = packet[UDP].dport
+
+        if sport == 53 or dport == 53:
             return "DNS"
 
-        if udp.sport in (67, 68) or udp.dport in (67, 68):
+        if sport == 67 or dport == 67:
             return "DHCP"
 
         return "UDP"
 
-    if packet.haslayer(IPv6):
-        return "IPv6"
-
-    if packet.haslayer(IP):
-        return "IPv4"
-
-    return "OTHER"
+    return packet.lastlayer().name
 
 
 def parse_packet(packet):
-    timestamp = datetime.fromtimestamp(
-        float(packet.time)
-    ).strftime("%H:%M:%S")
 
     source = "-"
     destination = "-"
-    source_port = "-"
-    destination_port = "-"
 
     if packet.haslayer(IP):
+
         source = packet[IP].src
         destination = packet[IP].dst
 
     elif packet.haslayer(IPv6):
+
         source = packet[IPv6].src
         destination = packet[IPv6].dst
 
-    elif packet.haslayer(ARP):
-        source = packet[ARP].psrc
-        destination = packet[ARP].pdst
+    source_port = "-"
+    destination_port = "-"
 
     if packet.haslayer(TCP):
+
         source_port = packet[TCP].sport
         destination_port = packet[TCP].dport
 
     elif packet.haslayer(UDP):
+
         source_port = packet[UDP].sport
         destination_port = packet[UDP].dport
 
     return {
-        "time": timestamp,
+
+        "time": datetime.now().strftime("%H:%M:%S"),
+
         "source": source,
         "destination": destination,
-        "protocol": get_protocol(packet),
+
+        "source_host": resolve_host(source),
+        "destination_host": resolve_host(destination),
+
+        "protocol": protocol_name(packet),
+
         "source_port": source_port,
         "destination_port": destination_port,
+
         "size": len(packet),
     }
